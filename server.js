28e9aea762423f294492ae62b21cb9cbce9e67ef
YashAnetz <55511290+YashAnetz@users.noreply.github.com>
2023-09-13 00:42:35 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,39 +1,23 @@
-// Budget API
-
 const express = require('express');
 const cors = require('cors');
+const fs = require('fs'); // Import the 'fs' module to work with the file system
 const app = express();
 const port = 3000;
 
 app.use(cors());
 
-const budget = {
-    myBudget: [
-        {
-            title: 'Eat out',
-            budget: 25
-        },
-        {
-            title: 'Rent',
-            budget: 275
-        },
-        {
-            title: 'Grocery',
-            budget: 110
-        },
-    ]
-};
+// Read the budget data from the JSON file
+const budget = JSON.parse(fs.readFileSync('budget.json'));
 
-app.use('/',express.static('public'))
+app.use('/', express.static('public'));
 
 app.get('/hello', (req, res) => {
     res.send('Hello World');
-    
 });
 
 app.get('/budget', (req, res) => {
+    // Send the budget data read from the JSON file as the response
     res.json(budget);
-    
 });
 
 app.listen(port, () => {
